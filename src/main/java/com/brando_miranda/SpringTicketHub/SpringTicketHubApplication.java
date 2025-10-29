0fea514f2e8f6a63b3aeee9460f4cebf9e0b51e4
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-10-29 15:11:05 -0300

    first commit

---
+++ src/main/java/com/brando_miranda/SpringTicketHub/SpringTicketHubApplication.java
@@ -0,0 +1,13 @@
+package com.brando_miranda.SpringTicketHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTicketHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTicketHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brando_miranda/SpringTicketHub/SpringTicketHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brando_miranda.SpringTicketHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTicketHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
